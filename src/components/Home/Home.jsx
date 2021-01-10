fe7fa7ab4c931e0180922c91d105b241565314b6
zenocoz <federico.soncini@gmail.com>
2021-01-10 19:54:11 +0100

    implemented control cycle with hooks

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,47 @@
 import React from "react"
 import { Container, Row, Col, Button, Form } from "react-bootstrap"
+import { useState, useEffect } from "react"
 import "./Home.css"
 
-export default function Home() {
+export default function Home(props) {
+  const [exam, setExam] = useState({})
+  const [submit, setSubmitted] = useState("")
+
+  const handleChange = (event) => {
+    setExam({ ...exam, [event.target.id]: event.target.value })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setSubmitted(exam)
+    console.log(exam)
+  }
+
   return (
     <Container className="homepage container-fluid">
       <Row>
         <Col>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group>
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter email" />
+              <Form.Control
+                id="name"
+                type="text"
+                placeholder="Enter email"
+                onChange={handleChange}
+                value={exam.name}
+              />
             </Form.Group>
 
             <Form.Group>
-              <Form.Label>Tipe of Exams</Form.Label>
-              <Form.Control type="text" placeholder="Enter type of exam" />
+              <Form.Label>Type of Exams</Form.Label>
+              <Form.Control
+                id="exam"
+                type="text"
+                placeholder="Enter type of exam"
+                onChange={handleChange}
+                value={exam.exam}
+              />
             </Form.Group>
             <span className="d-block text-center text-xl-left mr-xl-5">
               <Button
